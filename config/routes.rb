4380c36a0e520f8a4ae6ae89cb9b17b74205f5bb
lgf.lima@gmail.com <LuizGustavo>
2024-10-10 19:54:50 -0300

    nova atualização

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,21 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   
+  mount_devise_token_auth_for 'User', at: 'auth'
+
   # ROTAS MOVIE
-  resources :movies
+  resources :movies do
+    member do
+    # tudo definido aqui sera uma rota member
+     post 'like'
+    end
+  end
+
+  #post 'movies/:id\like', controller: 'movies', action: 'like'
+
   # # ROTAS STUDIO
   resources :studios
   # # ROTAS ATORES
   resources :actors
+
 end
